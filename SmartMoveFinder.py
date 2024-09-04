cea001f7d638e36d220dd6ed642f611b8bc9c03e
Gustavo Ogawa <gustavo.ogawa97@gmail.com>
2024-09-04 16:10:51 -0300

    1

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -1,9 +1,42 @@
 import random
 
+pieceScores = {'K': 0, 'Q': 10, 'R': 5, 'B': 3, 'N': 3, 'p': 1}
+checkmate = 1000
+stalemate = 0
+
+
 
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves) - 1)]
 
-def findBestMove():
-    return
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.whiteToMove else -1
+    maxScore = -checkmate
+    bestMove = None
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        if gs.checkmate:
+            score = checkmate
+        elif gs.stalemate:
+            score = 0
+        else:
+            score = turnMultiplier * scoreMaterial(gs.board)
+        if score > maxScore:
+            score = maxScore
+            bestMove = playerMove
+    return bestMove
+
+
+
+#Score the borad based on material.
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScores[square[1]]
+            elif square[0] == 'b':
+                score -=pieceScores[square[1]]
+    
+    return score
